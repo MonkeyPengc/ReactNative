d4840a1e34dc847711f2db32bb63a8f24bc2c776
Pengc <pengcheng612@gmail.com>
2020-02-20 08:58:26 -0800

    drawer navigator

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,39 +1,78 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import Dishdetail from './DishdetailComponent';
+import Home from './HomeComponent';
 import { View, Platform } from 'react-native';
-import { createStackNavigator } from 'react-navigation';
+import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
 
 
 const MenuNavigator = createStackNavigator({
-      Menu: { screen: Menu },
-      Dishdetail: { screen: Dishdetail }
-    },
-    {
-      initialRouteName: 'Menu',
-        navigationOptions: {
-            headerStyle: {
-                backgroundColor: "#512DA8"
-            },
-            headerTintColor: '#fff',
-            headerTitleStyle: {
-                color: "#fff"            
-            }
-        }
+  Menu: { screen: Menu },
+  Dishdetail: { screen: Dishdetail }
+},
+  {
+    initialRouteName: 'Menu',
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: "#512DA8"
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        color: "#fff"
+      }
     }
+  }
 );
+
+const HomeNavigator = createStackNavigator({
+  Home: { screen: Home }
+},
+  {
+    initialRouteName: 'Home',
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: "#512DA8"
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        color: "#fff"
+      }
+    }
+  }
+);
+
+const MainNavigator = createDrawerNavigator({
+  Home:
+  {
+    screen: HomeNavigator,
+    navigationOptions: {
+      title: 'Home',
+      drawerLabel: 'Home'
+    }
+  },
+  Menu:
+  {
+    screen: MenuNavigator,
+    navigationOptions: {
+      title: 'Menu',
+      drawerLabel: 'Menu'
+    },
+  }
+}, {
+    drawerBackgroundColor: '#D1C4E9'
+  });
 
 
 class Main extends Component {
-  
+
   render() {
- 
+
     return (
-        <View style={{flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
-          <MenuNavigator />
-        </View>
+      <View style={{ flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
+        <MainNavigator />
+      </View>
     );
   }
 }
-  
+
 export default Main;
